21da29e45f8614265fb0b4d44365f50397ab30a0
Shane Ramiah <shane@netfuz.com>
2020-04-08 09:42:20 +0400

    First chart

--- src/App.js
+++ src/App.js
@@ -1,20 +1,56 @@
 import React from 'react';
-import {Flex, Text, Box, Link} from "./components/base";
+import {Flex} from './components/base';
+import { Bar } from '@nivo/bar'
+import data from './data/stats.json';
 
 function App() {
-  return (
-      <Flex
-          px={2}
-          color='white'
-          bg='background'
-          alignItems='center'>
-        <Text p={2} fontWeight='bold'>Rebass</Text>
-        <Box mx='auto' />
-        <Link variant='nav' href='#!'>
-          Profile
-        </Link>
-      </Flex>
-  );
+
+    const vitals = data.map(d => {
+       return {
+           Recovered: d.recovered,
+           Death: d.death,
+           Active: d.active.total,
+           Date: d.date
+       }
+    });
+
+    console.log(vitals);
+
+    const keys = ['Death', 'Recovered', 'Active'];
+    const commonProps = {
+        width: 1000,
+        height: 500,
+        margin: {top: 60, right: 80, bottom: 60, left: 80},
+        data: vitals,
+        indexBy: 'Date',
+        keys,
+        padding: 0.5,
+        enableGridY: false,
+        labelTextColor: 'inherit:darker(1.4)',
+        labelSkipWidth: 16,
+        labelSkipHeight: 16,
+        enableLabel: false,
+        axisBottom: {
+            tickRotation: -45
+        }
+    };
+
+
+    return (
+        <Flex
+            px={2}
+            color="black"
+            bg="white"
+            alignItems="center">
+
+            <Bar
+                {...commonProps}
+                colors={{ scheme: 'set1' }}
+            />
+
+        </Flex>
+
+    );
 }
 
 export default App;
