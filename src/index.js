687a70425080847f0e7a62b7a292230bc840298e
Shane Ramiah <shane@netfuz.com>
2020-04-16 10:40:59 +0400

    Improve performance

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { hydrate, render }  from 'react-dom';
 import App from './App';
 import theme from "./theme";
 import * as serviceWorker from './serviceWorker';
 import { ThemeProvider } from 'styled-components'
 
 
-ReactDOM.render(
-  <React.StrictMode>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById('root');
+const app =
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <App />
+        </ThemeProvider>
+    </React.StrictMode>;
+
+if (rootElement.hasChildNodes()) {
+    hydrate(app, rootElement);
+} else {
+    render(app, rootElement);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
